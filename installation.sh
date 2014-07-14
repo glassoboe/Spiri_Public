69f840e9ce3236f6b4f98ba73b4d1e1bff4fb423
rohanbhargava11 <rohan.bhargava11@gmail.com>
2014-07-14 13:38:10 -0300

    fixed the catkin_make issue

--- installation.sh
+++ installation.sh
@@ -26,44 +26,8 @@ sudo apt-get install ros-hydro-desktop-full ros-hydro-hector-quadrotor ros-hydro
 # Configuring ros workspace
 
 echo "source /opt/ros/hydro/setup.bash" >> ~/.bashrc
-source ~/.bashrc
 
-mkdir -p ~/catkin_ws/src
-
-cd ~/catkin_ws/src
-
-catkin_init_workspace
-
-cd ..
-
-catkin_make
-
-echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
-source ~/.bashrc
-
-sudo rosdep init
-
-rosdep update
-# Download Spiri simulator
-
-cd ~/catkin_ws/src
-
-wget https://raw.github.com/Pleiades-Spiri/Spiri_Public/installation_simulator/Simulator-1.0.tar.gz
-
-tar -zxvf Simulator-1.0.tar.gz 
-
-mv Simulator-1.0 ./Simulator
-
-cd ..
-
-#Build the system
-
-catkin_make
-
-# Set Gazebo Model Path
-
-echo "export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:~/catkin_ws/src/Simulator/spiri_description/models" >> ~/.bashrc
-
+echo "Restart the terminal and run installation_simulator.sh"
 
 
 
