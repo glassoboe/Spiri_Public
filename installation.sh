2ca08fd573665f99d6aa73f212dfbb88606c6481
rohanbhargava11 <rohan.bhargava11@gmail.com>
2014-07-14 13:33:49 -0300

    for Ubuntu 12.10 and 13.04

--- installation.sh
+++ installation.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 # Script to install Spiri Simulator
+VER=$(lsb_release -sr)
 
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu precise main" > /etc/apt/sources.list.d/ros-latest.list'
+case "$VER" in 
+	"12.04") 
+	sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu precise main" > /etc/apt/sources.list.d/ros-latest.list'
+	;;
+	"12.10") 
+	sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu quantal main" > /etc/apt/sources.list.d/ros-latest.list'
+	;;
+	"13.04")
+	sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu raring main" > /etc/apt/sources.list.d/ros-latest.list'
+
+
+	
+esac
 
 wget https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -O - | sudo apt-key add -
 
 sudo apt-get update
-
 # Install all the ROS required packages
 
 sudo apt-get install ros-hydro-desktop-full ros-hydro-hector-quadrotor ros-hydro-ros-controllers ros-hydro-joy
@@ -51,11 +63,6 @@ catkin_make
 # Set Gazebo Model Path
 
 echo "export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:~/catkin_ws/src/Simulator/spiri_description/models" >> ~/.bashrc
-
-
-
-
-
 
 
 
